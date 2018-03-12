6edf1dbbc2c8e0df92e69dd8f9ba60539d3e4002
Alexander Lexis <Alexander@macbooklexander.home>
2018-03-12 16:23:17 +0100

    Database connected en schepen invoeren werkt

--- schepenmaken.php
+++ schepenmaken.php
@@ -9,9 +9,20 @@
 
 <?php
 
+
+$servername = 'localhost;';
+$username = 'root';
+$password = 'root';
+$db = 'ZeeslagNickAlex';
+
+$conn = mysqli_connect ($servername, $username, $password, $db);
+
+
+
 echo '<input type = "text" placeholder = "Hoe heet je schip?" id="schipnaam">';
 echo '<input type = "text" placeholder = "Hoe lang is je schip?" id="schiplengte">';
 echo '<input type = "button" value = "Versturen" onclick = versturen()>';
+
 
 /* 
  * To change this license header, choose License Headers in Project Properties.
