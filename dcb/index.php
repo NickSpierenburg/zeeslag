4c6f1f90e275d23a2eb3e555364ab52e89ed8a54
Geert Schippers <g.j.f.m.@gmail.com>
2018-03-28 13:33:46 +0200

    rekening aanmaken werkt

--- dcb/index.php
+++ dcb/index.php
@@ -18,6 +18,12 @@ echo 'Welkom, ' . $user->getVolledigeNaam();
 echo '<br>';
 echo 'Uw gekozen gebruikersnaam is ' . $user->getGebruikersnaam() . ' met als wachtwoord ' . $user->getWachtwoord();
 echo '<hr>';
-// $user->maakGebruiker($conn); //WERKT!
+//$user->maakGebruiker($conn); //WERKT!
 
+$rekening = new rekening('MCB12345', 'Admin', 5000);
+echo 'Uw nieuwe rekenings nummer is '. $rekening->getRekeningNummer();
+echo '<br>';
+echo 'Met uw totaale balans is :'. $rekening->getBalans();
+echo '<br>';
+//$rekening->maakRekening($conn); //WERKT OOK
 ?>
